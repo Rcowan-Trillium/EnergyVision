efa8d87af39a8903426fd1501abb1a1b04baace7
Rcowan-Trillium <riley.cowan@trilliumhcp.com>
2025-11-12 08:43:43 -0500

    removed .vs from git folder and added .gitignore

--- EnergyHistory/obj/Release/net8.0-windows/EnergyHistory.AssemblyInfo.vb
+++ EnergyHistory/obj/Release/net8.0-windows/EnergyHistory.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("EnergyHistory"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad79c2adaabbb0084aee452d02163b5e7b04f556"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064a3a506364324bcee3ff0aeef7985a0124892f"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("EnergyHistory"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("EnergyHistory"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
